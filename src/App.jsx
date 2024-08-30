af5b4eb61d0e064a9d8520407a0c114266156afd
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-08-30 20:58:50 +1000

    added new design

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <Categories title="Shop by Category" linkUrl="#" linkText="View all categories" />
-      <Categories title="New Category" linkUrl="#" />
+      
     </div>
     
       
---
+++ src/data/productData.js
@@ -0,0 +1,32 @@
+const productData = {
+    categories: [
+        {
+            id: 1,
+            category: "New Arrival",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg"
+        },
+        {
+            id: 2,
+            category: "Productivity",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg"
+        },
+        {
+            id: 3,
+            category: "Workspace",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg"
+        },
+        {
+            id: 4,
+            category: "Accessories",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg"
+        },
+        {
+            id: 5,
+            category: "Sale",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg"
+        }
+    ]
+
+}
+
+export default productData;
