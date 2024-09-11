8a9ab5a29dd8a9aaa30801f9b047a47c4afa95fb
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-09-11 22:40:37 +1000

    added name

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,9 +1,43 @@
 const productData = {
     categories: [
         {
-            id: 1,
+            id: "new-arrivals",
             category: "New Arrival",
-            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg"
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
+
+            products: [
+                {
+                    
+                    name: "Harper dress",
+                    price: "49$",
+                    offer: "SAVE 30%", 
+                    href: "#",
+                    imageAlt: "",
+                    imageSrc: "https://www.davidjones.com/productimages/thumb/2/2730644_23146453_15040001.jpg",
+
+                },
+                
+                {
+                    
+                    name: "Midi dress",
+                    price: "79$",
+                    href: "#",
+                    imageAlt: "",
+                    offer: "SAVE 30%",
+                    imageSrc: "https://dollygirlfashion.com/cdn/shop/files/CM4048D07_Light_Denim_Dress_1_720x.jpg?v=1710036927",
+
+                },
+                {
+                    
+                    name: "Beach dress",
+                    price: "59$",
+                    offer: "SAVE 30%",
+                    href: "#",
+                    imageAlt: "",
+                    imageSrc: "https://dollygirlfashion.com/cdn/shop/files/CM4048D07_Light_Denim_Dress_1_720x.jpg?v=1710036927",
+
+                },
+            ]
         },
         {
             id: 2,
